e4aef248a62bd5374b4f93868c7d063211f4e407
Koreczek <P.Skorupka@stud.elka.pw.edu.pl>
2021-08-06 15:17:14 +0200

    initialize project

---
+++ password_generator.cpp
@@ -0,0 +1,12 @@
+﻿// password_generator.cpp : Defines the entry point for the application.
+//
+
+#include "password_generator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
